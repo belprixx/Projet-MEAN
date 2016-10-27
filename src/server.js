df0cdad1c7353a5321938a4bf0d63abd848c7a03
Samuel <samuel.thomas@hotmail.fr>
2016-10-27 15:40:50 +0200

    debut de front avec css et js

--- src/server.js
+++ src/server.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.set('view engine', 'jade');
-app.get('/', function(req, res) {
-    res.sendfile('./src/public/index.html')
-});
+app.use("/", express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
