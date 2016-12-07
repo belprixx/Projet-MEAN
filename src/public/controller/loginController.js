8db7141083021feb7114c964756cece492387ae0
Al-Drac <remi.marchal6@gmail.com>
2016-12-07 08:55:25 +0100

    gestion coordonees, fonction update

--- src/public/controller/loginController.js
+++ src/public/controller/loginController.js
@@ -1,12 +1,8 @@
 angular.module('BostonApp').controller('loginController', function($scope, userFactory, $location) {
 	$scope.submitForm = function(form) {
 			if (form.$valid) {
-<<<<<<< HEAD
-					userFactory.setUsername($scope.userName).login();
-=======
 				var login = userFactory.setUsername($scope.userName).login();
->>>>>>> 96afc8e1ecdf9e094c173ec7da823856bfd39afe
-					$location.url('/');
+				$location.url('/');
 			} else {
 					alert('Invalide');
 			}
