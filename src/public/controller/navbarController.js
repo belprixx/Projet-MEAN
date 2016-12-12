e6177ffcd6c3c15851ea1116168a814862f9b9dc
keitsu <samuel.thomas@hotmail.fr>
2016-12-12 16:43:38 +0100

    sidebar apparait et disparait si connecté mais css de la barre merde

--- src/public/controller/navbarController.js
+++
@@ -1,7 +0,0 @@
-angular.module('BostonApp').controller('headerController', function($scope, userFactory) {
-    $scope.$watch(function() {
-        return userFactory.isSignedIn();
-    }, function(logged) {
-        $scope.connected = logged;
-    });
-});
